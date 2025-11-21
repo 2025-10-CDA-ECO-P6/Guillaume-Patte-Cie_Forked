9a4e7d7521a4aa63466e7cd5af74a4fa284c79ca
guibrok <gbroquet@gmail.com>
2025-11-21 09:18:09 +0100

    Update animal photo path, enhance AnimalCard styles, and clean up global CSS

--- src/app/components/AnimalCard/animalCard.js
+++ src/app/components/AnimalCard/animalCard.js
@@ -3,19 +3,22 @@ import styles from "./animalCard.module.css";
 
 export default function AnimalCard({ animal }) {
   return (
-    <Link href={`/animals/${animal.id}`} className={styles["animal-card"]}>
-      <div className="animal-card-content">
+    <Link href={`/animals/${animal.id}`} className={styles.link}>
+      <div className={styles["animal-card"]}>
+        
         <img
           src={`/${animal.photo}`}
           alt={animal.nom}
-          className="animal-photo"
+          className={styles["animal-photo"]}
         />
 
-        <div className="animal-info">
+        <div className={styles["animal-info"]}>
           <h3>{animal.nom}</h3>
           <p>{animal.espece}</p>
         </div>
+
       </div>
     </Link>
   );
 }
+
